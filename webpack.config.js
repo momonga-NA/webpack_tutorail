d3a137d2aeb8bc179da76cb0ad6ae46de1b73cde
N.Atsuki <43095875+momonga-NA@users.noreply.github.com>
2021-03-05 18:54:56 +0900

    apply MiniCssExtractPlugin & HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -15,7 +17,7 @@ module.exports = {
         // 定義したloaderは下から適用される
         use: [
           {
-            loader: 'style-loader'
+            loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: 'css-loader'
@@ -24,4 +26,10 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
