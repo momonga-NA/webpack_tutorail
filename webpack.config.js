2bbcc05fa03737490470039a307c45256c96f4cf
N.Atsuki <43095875+momonga-NA@users.noreply.github.com>
2021-03-07 16:38:43 +0900

    apply typescript

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,27 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(ts|tsx)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+
+          },
+        ],
+      },
+      {
         test: /\.js/,
         exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: [
+                ['@babel/preset-env'],
+                '@babel/preset-react',
+
+              ]
             },
           },
         ],
